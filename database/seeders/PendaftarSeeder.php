b9399de12c99f3963665da05b9c40ac13e2b5284
Ridho Hauzan <dotcomhomo@gmail.com>
2021-11-07 16:23:29 +0700

    PendaftarSeeder

--- database/seeders/PendaftarSeeder.php
+++ database/seeders/PendaftarSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
 
 class PendaftarSeeder extends Seeder
 {
@@ -13,6 +16,19 @@ class PendaftarSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create('id_ID');
+        for($i = 1; $i <= 5; $i++){
+            // insert data ke table siswa menggunakan Faker
+            \DB::table('pendaftar_kerjas')->insert([
+                'lowonganId' => $faker->numberBetween($min = 0000, $max = 9999),
+                'namaPendaftar' => $faker->name,
+                'email' => $faker->email,
+                'alamat' => $faker->address,
+                'telephone' => $faker->phoneNumber,
+                'pendidikanTerakhir' => $faker->word,
+                'cv' => $faker->word,
+                'ijazahTerakhir' => $faker->word,
+            ]);
     }
+}
 }
