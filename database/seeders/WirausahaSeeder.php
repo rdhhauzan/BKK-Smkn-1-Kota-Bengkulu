e289de2cb18ae29be1ef9783387275118af96c62
Ridho Hauzan <dotcomhomo@gmail.com>
2021-11-07 16:25:29 +0700

    WirausahaSeeder

---
+++ database/seeders/WirausahaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
+class WirausahaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        for($i = 1; $i <= 5; $i++){
+            // insert data ke table siswa menggunakan Faker
+            \DB::table('wirausahas')->insert([
+                'alumniId' => $faker->numberBetween($min = 0000, $max = 9999),
+                'namaUsaha' => $faker->company,
+                'bidangUsaha' => $faker->jobTitle,
+                'tahunMasuk' => $faker->year
+                ]);
+    }
+}
+}
