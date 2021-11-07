ebbcb7eaaead190352486429e8a04923fe6478ab
Ridho Hauzan <dotcomhomo@gmail.com>
2021-11-07 16:19:23 +0700

    PekerjaanSeeder

---
+++ database/seeders/PekerjaanSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
+class PekerjaanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        for($i = 1; $i <= 5; $i++){
+            // insert data ke table siswa menggunakan Faker
+            \DB::table('pekerjaans')->insert([
+                'alumniId' => $faker->numberBetween($min = 0000, $max = 9999),
+                'tempatKerja' => $faker->city,
+                'jabatan' => $faker->jobTitle,
+                'tahunMasuk' => $faker->numberBetween($min = 2000, $max = 2021),
+                'tahunKeluar' => $faker->numberBetween($min = 0000, $max = 2021),
+                'alamatKerja' => $faker->address,
+            ]);
+    }
+}
+}
